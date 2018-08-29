52ab2a11867f1796a7a2dafed20d8db372c86fc8
Donald Oakes <donald.oakes@centurylink.com>
2018-08-29 17:16:30 -0600

    cleanup

--- src/com/centurylink/mdw/studio/action/AssetVersionAction.kt
+++ src/com/centurylink/mdw/studio/action/AssetVersionAction.kt
@@ -1,9 +1,7 @@
 package com.centurylink.mdw.studio.action
 
-import com.centurylink.mdw.studio.file.Asset
 import com.centurylink.mdw.studio.file.AssetPackage
 import com.centurylink.mdw.studio.proj.ProjectSetup
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.LangDataKeys

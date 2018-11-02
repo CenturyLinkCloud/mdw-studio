b19e274f2354735437fd84c9cee5fb905961e0ab
naveena-bellamkonda <naveena.bellamkonda@centurylink.com>
2018-11-02 20:40:33 +0530

    issue #36 (#38)

--- mdw-studio/src/com/centurylink/mdw/studio/action/ExportProcess.kt
+++ mdw-studio/src/com/centurylink/mdw/studio/action/ExportProcess.kt
@@ -1,17 +1,23 @@
 package com.centurylink.mdw.studio.action
 
 import com.centurylink.mdw.bpmn.BpmnProcessExporter
+import com.centurylink.mdw.cli.Download
 import com.centurylink.mdw.export.ProcessExporter
 import com.centurylink.mdw.html.HtmlProcessExporter
 import com.centurylink.mdw.image.PngProcessExporter
 import com.centurylink.mdw.model.workflow.Process
+import com.centurylink.mdw.pdf.PdfProcessExporter
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.fileChooser.FileChooserFactory
 import com.intellij.openapi.fileChooser.FileSaverDescriptor
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.progress.util.ProgressIndicatorBase
+import com.intellij.util.lang.UrlClassLoader
 import org.json.JSONObject
+import java.io.File
 import java.io.IOException
+import java.net.URL
 
 class ExportProcess : AssetAction() {
 
@@ -22,6 +28,7 @@ class ExportProcess : AssetAction() {
             val ext = when (templatePresentation.text) {
                 "PNG Image" -> "png"
                 "HTML Document" -> "html"
+                "PDF Document" -> "pdf"
                 else -> "bpmn"
             }
             locator.getAsset()?.let { asset ->
@@ -39,6 +46,29 @@ class ExportProcess : AssetAction() {
                             "html" -> {
                                 val exp = HtmlProcessExporter(projectSetup)
                                 exp.setOutputDir(fileWrapper.file.parentFile)
+                                exp
+                            }
+                            "pdf" -> {
+                                val tempDir = File(projectSetup.project.basePath + "/.temp")
+                                if (!tempDir.exists())
+                                    tempDir.mkdirs()
+                                val itextJar = File(tempDir.path + "/itextpdf-5.5.13.jar")
+                                if (!itextJar.isFile) {
+                                    Download(URL("http://repo.maven.apache.org/maven2/com/itextpdf/itextpdf/5.5.13/itextpdf-5.5.13.jar"),
+                                            itextJar, 2320581L).run(com.centurylink.mdw.studio.ui.ProgressMonitor(ProgressIndicatorBase()))
+                                }
+                                val xmlWorkerJar = File(tempDir.path + "/xmlworker-5.5.13.jar")
+                                if (!xmlWorkerJar.isFile) {
+                                    Download(URL("http://repo.maven.apache.org/maven2/com/itextpdf/tool/xmlworker/5.5.13/xmlworker-5.5.13.jar"),
+                                            xmlWorkerJar, 2320581L).run(com.centurylink.mdw.studio.ui.ProgressMonitor(ProgressIndicatorBase()))
+                                }
+                                val classLoader = this.javaClass.classLoader as UrlClassLoader
+                                val method = UrlClassLoader::class.java.getDeclaredMethod("addURL", URL::class.java)
+                                method.isAccessible = true
+                                method.invoke(classLoader, itextJar.toURI().toURL())
+                                method.invoke(classLoader, xmlWorkerJar.toURI().toURL())
+                                val exp = PdfProcessExporter(projectSetup)
+                                exp.setOutputDir(fileWrapper.file)
                                 exp
                             }
                             else -> {

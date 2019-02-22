18acd52d7193d995840d7ba118bf9dff68bf24ce
Jack Brojde <Jack.Brojde@centurylink.com>
2019-02-22 15:23:11 -0700

    Issue #61

--- mdw-studio/src/com/centurylink/mdw/studio/proj/AssetFileListener.kt
+++ mdw-studio/src/com/centurylink/mdw/studio/proj/AssetFileListener.kt
@@ -9,6 +9,8 @@ import com.centurylink.mdw.studio.file.AssetPackage
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileCopyEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
@@ -24,83 +26,89 @@ class AssetFileListener(private val projectSetup: ProjectSetup) : BulkFileListen
     }
 
     override fun after(events: MutableList<out VFileEvent>) {
-        for (event in events) {
-            getAssetEvent(event)?.let { assetEvent ->
-                LOG.debug("Asset event: $assetEvent")
-                val asset = assetEvent.asset
-                when (assetEvent.type) {
-                    EventType.Create -> {
-                        projectSetup.setVersion(asset, 1)
-                    }
-                    EventType.Update, EventType.Copy, EventType.Move -> {
-                        // TODO handle large files
-                        val increment = projectSetup.git?.let { git ->
-                            if (asset.name.endsWith(".impl")) {
-                                projectSetup.reloadImplementors()
-                            }
-                            else if (asset.name.endsWith(".evth")) {
-                                // not asset
-                            }
-                            else if (asset.version == 0) {
-                                projectSetup.setVersion(asset, 1)
-                            }
-                            else if (FileUtilRt.isTooLarge(asset.file.length)) {
-                                LOG.info("Skip vercheck for large asset: $asset")
-                            }
-                            else {
-                                LOG.debug("Performing vercheck: $asset")
-                                val gitPkgPath = git.getRelativePath(File(asset.pkg.dir.path).toPath())
-                                val gitBytes = git.readFromHead("$gitPkgPath/${asset.name}")
-                                val assetBytes = asset.file.contentsToByteArray()
-                                var isGitDiff = false
-                                if (gitBytes != null && !Arrays.equals(gitBytes, assetBytes)) {
-                                    isGitDiff = if (Data.getBinaryAssetExts(projectSetup).contains(asset.ext)) {
-                                        true
+        if (!isVcsOperationInProgress(ProjectLevelVcsManager.getInstance(projectSetup.project))) {
+            for (event in events) {
+                getAssetEvent(event)?.let { assetEvent ->
+                    LOG.debug("Asset event: $assetEvent")
+                    val asset = assetEvent.asset
+                    when (assetEvent.type) {
+                        EventType.Create -> {
+                            projectSetup.setVersion(asset, 1)
+                        }
+                        EventType.Update, EventType.Copy, EventType.Move -> {
+                            // TODO handle large files
+                            projectSetup.git?.let { git ->
+                                if (asset.name.endsWith(".impl")) {
+                                    projectSetup.reloadImplementors()
+                                } else if (asset.name.endsWith(".evth")) {
+                                    // not asset
+                                } else if (asset.version == 0) {
+                                    projectSetup.setVersion(asset, 1)
+                                } else if (FileUtilRt.isTooLarge(asset.file.length)) {
+                                    LOG.info("Skip vercheck for large asset: $asset")
+                                } else {
+                                    LOG.debug("Performing vercheck: $asset")
+                                    val gitPkgPath = git.getRelativePath(File(asset.pkg.dir.path).toPath())
+                                    val gitBytes = git.readFromHead("$gitPkgPath/${asset.name}")
+                                    val assetBytes = asset.file.contentsToByteArray()
+                                    var isGitDiff = false
+                                    if (gitBytes != null && !Arrays.equals(gitBytes, assetBytes)) {
+                                        isGitDiff = if (Data.getBinaryAssetExts(projectSetup).contains(asset.ext)) {
+                                            true
+                                        } else {
+                                            // ignore line ending diffs
+                                            val gitString = String(gitBytes).replace("\r", "")
+                                            val assetString = String(assetBytes).replace("\r", "")
+                                            gitString != assetString
+                                        }
                                     }
-                                    else {
-                                        // ignore line ending diffs
-                                        val gitString = String(gitBytes).replace("\r", "")
-                                        val assetString = String(assetBytes).replace("\r", "")
-                                        gitString != assetString
+                                    if (isGitDiff) {
+                                        val gitVerFileBytes = git.readFromHead("$gitPkgPath/${AssetPackage.VERSIONS_FILE}")
+                                        gitVerFileBytes?.let {
+                                            val gitVerProps = Properties()
+                                            gitVerProps.load(ByteArrayInputStream(gitVerFileBytes))
+                                            val gitVerProp = gitVerProps.getProperty(asset.name)
+                                            if (gitVerProp != null) {
+                                                val headVer = gitVerProp.split(" ")[0].toInt()
+                                                if (headVer >= asset.version) {
+                                                    projectSetup.setVersion(asset, headVer + 1)
+                                                }
+                                            }
+                                        }
                                     }
-                                }
-                                if (isGitDiff) {
-                                    val gitVerFileBytes = git.readFromHead("$gitPkgPath/${AssetPackage.VERSIONS_FILE}")
-                                    gitVerFileBytes?.let {
-                                        val gitVerProps = Properties()
-                                        gitVerProps.load(ByteArrayInputStream(gitVerFileBytes))
-                                        val gitVerProp = gitVerProps.getProperty(asset.name)
-                                        if (gitVerProp != null) {
-                                            val headVer = gitVerProp.split(" ")[0].toInt()
-                                            if (headVer >= asset.version) {
-                                                projectSetup.setVersion(asset, headVer + 1)
+                                    if (asset.name.endsWith(".java") || asset.name.endsWith(".kt")) {
+                                        DumbService.getInstance(projectSetup.project).smartInvokeLater {
+                                            projectSetup.findPsiAnnotation(asset, Activity::class)?.let { psiAnnotation ->
+                                                Implementors.getImpl(projectSetup, asset, psiAnnotation)?.let { projectSetup.reloadImplementors() }
                                             }
                                         }
                                     }
                                 }
-                                if (asset.name.endsWith(".java") || asset.name.endsWith(".kt")) {
-                                    DumbService.getInstance(projectSetup.project).smartInvokeLater {
-                                        projectSetup.findPsiAnnotation(asset, Activity::class)?.let { psiAnnotation ->
-                                            Implementors.getImpl(projectSetup, asset, psiAnnotation)?.let { projectSetup.reloadImplementors() }
-                                        }
-                                    }
+                            }
+                            if (event is VFileMoveEvent) {
+                                projectSetup.getPackage(event.oldParent)?.let { oldPkg ->
+                                    val oldAsset = Asset(oldPkg, LightVirtualFile(event.file.name))
+                                    projectSetup.setVersion(oldAsset, 0)
                                 }
                             }
                         }
-                        if (event is VFileMoveEvent) {
-                            projectSetup.getPackage(event.oldParent)?.let { oldPkg ->
-                                val oldAsset = Asset(oldPkg, LightVirtualFile(event.file.name))
-                                projectSetup.setVersion(oldAsset, 0)
-                            }
+                        EventType.Delete -> {
+                            projectSetup.setVersion(asset, 0)
                         }
-                    }
-                    EventType.Delete -> {
-                        projectSetup.setVersion(asset, 0)
-                    }
-                    EventType.Unknown -> {
+                        EventType.Unknown -> {
+                        }
                     }
                 }
             }
+        }
+    }
+
+    private fun isVcsOperationInProgress(vcsManager: ProjectLevelVcsManager) : Boolean {
+        if (vcsManager is ProjectLevelVcsManagerImpl) {
+            return vcsManager.isBackgroundVcsOperationRunning()
+        }
+        else {
+            return false
         }
     }
 

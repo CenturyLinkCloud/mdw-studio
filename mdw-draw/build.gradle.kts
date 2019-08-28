4892a409409b04b0f3ab0902b4b7f40a31e61826
Donald Oakes <donald.oakes@centurylink.com>
2019-08-28 15:00:30 -0600

    prep merge master

--- mdw-draw/build.gradle.kts
+++ mdw-draw/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
-    compile("com.centurylink.mdw:mdw-common:6.1.23-SNAPSHOT") { isTransitive = false }
+    compile("com.centurylink.mdw:mdw-common:6.1.25-SNAPSHOT") { isTransitive = false }
     // compile(files("../../mdw/mdw/deploy/app/mdw-common-6.1.23-SNAPSHOT.jar"))
     compile("io.limberest:limberest:1.2.5") { isTransitive = false }
     compile("org.json:json:20180130")
